2e855a54f963824d360b4cfa73a456e69c21509c
User Androsenko <urij@MacBook-Air-Urij.local>
2026-06-16 22:36:31 +0200

    дз

--- src/main/java/ru/yandex/practicum/filmorate/validation/MinReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/MinReleaseDate.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = MinReleaseDateValidator.class)
 public @interface MinReleaseDate {
-    String message() default "Дата релиза должна быть не раньше 28.12.1895";
+    String message() default "Дата релиза должна быть не раньше 28-12-1895";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
     String min() default "1895-12-28";

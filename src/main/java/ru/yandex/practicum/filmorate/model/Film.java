3d32d7270ef1c22502f7759d8ffe50f969ce674a
User Androsenko <urij@MacBook-Air-Urij.local>
2026-06-21 23:10:05 +0200

    дз

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
-import lombok.NonNull;
 import lombok.Builder;
 import lombok.Data;
 import ru.yandex.practicum.filmorate.validation.MinReleaseDate;

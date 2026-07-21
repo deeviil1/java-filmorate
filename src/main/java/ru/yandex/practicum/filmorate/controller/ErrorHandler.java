aad7a0b355920f74daf5cc714041a35fb5ec7fe2
User Androsenko <urij@MacBook-Air-Urij.local>
2026-07-21 20:04:59 +0200

    Новый функционал добавление в друзья и лайков fix

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -22,7 +22,7 @@ public class ErrorHandler {
         return new ErrorResponse("Объект не найден", e.getMessage());
     }
 
-    @ExceptionHandler(Throwable.class)
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
         return new ErrorResponse("Произошла непредвиденная ошибка", e.getMessage());
